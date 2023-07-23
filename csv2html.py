935416dfd506e3b6ff7b66fa92af5f0a5456f4b3
F-Depi <fdp.federico@gmail.com>
2023-07-23 18:13:41 +0200

    finally fixed position index

--- csv2html.py
+++ csv2html.py
@@ -11,17 +11,17 @@ def csv2html(input_folder):
             html_file = os.path.join(output_folder, filename.replace(".csv", ".html"))
 
             df = pd.read_csv(csv_file, dtype=str)
-            df.insert(0, '#', range(1, len(df) + 1))  # Inserisci l'indice personalizzato nella colonna '#'
+            df.insert(0, '#', range(1, len(df) + 1))    # Position index in the column '#'
+            for ii in range(len(df) - 1):               # fix index increase beased on result column
+                if df.iat[ii, 1] == df.iat[ii+1, 1]:
+                    df.iat[ii+1, 0] = df.iat[ii, 0]
 
             html_table = df.to_html(index=False, na_rep='', classes=['centered-table'], justify='left')
 
             with open(html_file, 'w') as f:
                 f.write(html_table)
 
-# let's run it on all the folders
-#for ii in ["P", "I"]:
-#    for jj in ["M", "F"]:
-#        csv2html("database_ANA/assoluti/database_"+jj+"_"+ii+"_bl012/lists")
+
 
 for ii in ["P", "I"]:
     for jj in ["M", "F"]:
